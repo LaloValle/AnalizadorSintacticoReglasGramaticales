0040d85b057400d77a924bac867fb70736e5d967
Lalo Valle <lvalle212@gmail.com>
2020-03-24 02:15:54 -0600

    Analizador sintáctico con reglas gramaticales para reglas gramáticales y analizador LL1 de una regla completado, realiza el analisis de una cadena según sea evaluada con las reglas ingresadas en el archivo de reglas

---
+++ PruebaReglas.py
@@ -0,0 +1,67 @@
+# -*- coding: utf-8 -*-
+#@author: Lalo Valle
+
+from AnalizadorLexico import *
+from AnalizadorSintactico import *
+from GeneradorAutomatas import *
+
+import sys
+
+rutaReglas = sys.argv[1]
+
+with open(rutaReglas,'r') as archivo:
+	cadena = archivo.read()
+
+if cadena.find('\n'):
+	cadena = cadena.replace('\n','')
+
+tabular = ManejadorTabulares.recuperarTabular('TabularReglas.dat')
+
+automata = ManejadorTabulares.generarAFDDeTabular(tabular)
+
+lexico = AnalizadorLexico(automata, cadena)
+
+sintactico = AnalizadorSintacticoReglas(lexico)
+
+resultado = -1
+
+valido = sintactico.analizar()
+
+if valido:
+	print('\n gramatica reconocida\n')
+	gramatica = sintactico.getGramaticaGenerada()
+	gramatica.imprimirGramaticaConsola()
+
+	print('\n ahora sin recursión\n')
+
+	gramatica.eliminarRecursionIzquierda()
+	gramatica.imprimirGramaticaConsola()
+
+	print('\n Simbolos terminales y no terminales\n')
+
+	print(gramatica.getSimbolosNoTerminales())
+	print(gramatica.getSimbolosTerminales())
+
+	print('\n first de E\n')
+	ll1 = AnalizadorSintacticoLL1(gramatica)
+	resultado = ll1.first('E')
+	print(resultado)
+	print()
+
+	print('\n follow de Tp\n')
+	resultado = ll1.follow('Tp')
+	print(resultado)
+	print()
+
+	print('\n tabla LL1\n')
+	ll1.crearTablaLL1()
+
+	cadena = '(SorS)?°S+°S°S+°((SorS)°(SorS)?°S+)'
+	ll1.setCadena(cadena)
+	print('\n analisis de la cadena {}\n'.format(cadena))
+	valido,error = ll1.analizar()
+	print(valido)
+	print(error)
+
+else:
+	print('Error')
